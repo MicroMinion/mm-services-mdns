a7a5cc160de3fc3b671e845019cfb7ceadc7bfae
Thomas Delaet <thomas@delaet.org>
2016-06-19 22:56:39 +0200

    support 1tp format of connectionInfo and new mm-platform format of nodeInfo

--- index.js
+++ index.js
@@ -25,59 +25,81 @@ var mDNSService = function (options) {
       var addresses = data.addresses
       var signId = data.host.split('.')[0]
       var boxId = data.host.split('.')[1]
-      var connectionInfo = {
+      var nodeInfo = {
         signId: signId,
-        boxId: boxId
+        boxId: boxId,
+        connectionInfo: []
       }
       if (isFlunky(data, 'tcp')) {
-        connectionInfo.tcp = {
-          address: addresses[0],
-          port: port
-        }
+        _.forEach(addresses, function (address) {
+          nodeInfo.connectionInfo.push({
+            transportType: 'tcp',
+            transportInfo: {
+              port: port,
+              address: address
+            }
+          })
+        })
       }
       if (isFlunky(data, 'udp')) {
-        connectionInfo.udp = {
-          address: addresses[0],
-          port: port
-        }
+        _.forEach(addresses, function (address) {
+          nodeInfo.connectionInfo.push({
+            transportType: 'udp',
+            transportInfo: {
+              port: port,
+              address: address
+            }
+          })
+        })
       }
-      service.hosts[signId] = connectionInfo
-      debug(connectionInfo)
-      service.messaging.send('transports.connectionInfo', 'local', connectionInfo)
-      service.messaging.send('transports.connectionInfoBootstrap', 'local', connectionInfo)
+      service.hosts[signId] = nodeInfo
+      debug(nodeInfo)
+      service.messaging.send('transports.nodeInfo', 'local', nodeInfo)
+      service.messaging.send('transports.nodeInfoBootstrap', 'local', nodeInfo)
     }
   })
-  this.messaging.on('self.transports.myConnectionInfo', this._update.bind(this))
-  this.messaging.on('self.transports.requestConnectionInfo', this._request.bind(this))
-  this.messaging.on('self.transports.requestBootstrapConnectionInfo', this._requestAll.bind(this))
+  this.messaging.on('self.transports.myNodeInfo', this._update.bind(this))
+  this.messaging.on('self.transports.requestNodeInfo', this._request.bind(this))
+  this.messaging.on('self.transports.requestBootstrapNodeInfo', this._requestAll.bind(this))
 }
 
 mDNSService.prototype._requestAll = function (topic, local, data) {
   debug('_requestAll')
   var self = this
   debug(this.hosts)
-  _.forEach(this.hosts, function (connectionInfo) {
-    self.messaging.send('transports.connectionInfoBootstrap', 'local', connectionInfo)
+  _.forEach(this.hosts, function (nodeInfo) {
+    self.messaging.send('transports.nodeInfoBootstrap', 'local', nodeInfo)
   }, this)
 }
 
 mDNSService.prototype._update = function (topic, publicKey, data) {
   debug('_update')
-  this.connectionInfo = data
-  if (_.has(this.connectionInfo, 'tcp')) {
+  this.nodeInfo = data
+  var mdns = this
+  if (_.some(this.nodeInfo.connectionInfo, function (transport) {
+      return transport.transportType === 'tcp'
+    })) {
     if (this.serviceTcp) {
       this.serviceTcp.stop()
     }
-    this.serviceTcp = mdns.createAdvertisement(mdns.tcp('flunky'), this.connectionInfo.tcp.port, {
+    var port = _.find(this.nodeInfo.connectionInfo, function (transport) {
+      return transport.transportType === 'tcp'
+    }).transportInfo.port
+    this.serviceTcp = mdns.createAdvertisement(mdns.tcp('flunky'), port, {
       name: data.signId + '.' + data.boxId
     })
     this.serviceTcp.start()
   }
-  if (_.has(this.connectionInfo, 'udp')) {
+  if (_.some(this.nodeInfo.connectionInfo, function (transport) {
+      return transport.transportType === 'udp'
+    })) {
     if (this.serviceUdp) {
       this.serviceUdp.stop()
     }
-    this.serviceUdp = mdns.createAdvertisement(mdns.udp('flunky'), this.connectionInfo.udp.port, {
+    port = _.find(this.nodeInfo.connectionInfo, function (transport) {
+      return transport.transportType === 'udp'
+    }).transportInfo.port
+    this.serviceUdp = mdns.createAdvertisement(mdns.udp('flunky'), port, {
       name: data.signId + '.' + data.boxId
     })
     this.serviceUdp.start()
@@ -87,7 +109,7 @@ mDNSService.prototype._update = function (topic, publicKey, data) {
 mDNSService.prototype._request = function (topic, publicKey, data) {
   debug('_request')
   if (_.has(this.hosts, data)) {
-    this.messaging.send('transports.connectionInfo', 'local', this.hosts[data])
+    this.messaging.send('transports.nodeInfo', 'local', this.hosts[data])
   }
 }
 
