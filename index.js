c34aaddaf6012a4f59dfaedb67f5533fd39fe368
Thomas Delaet <thomas@delaet.org>
2016-08-01 17:56:15 +0200

    take out debug module

--- index.js
+++ index.js
@@ -2,10 +2,8 @@
 
 var MulticastDNS = require('multicast-dns')
 var _ = require('lodash')
-var debug = require('debug')('mm-services:mdns')
 
 var mDNSService = function (options) {
-  debug('initialize')
   var self = this
   this.hosts = {}
   this.messaging = options.platform.messaging
@@ -13,10 +11,8 @@ var mDNSService = function (options) {
     loopback: true
   })
   this.mdns.on('response', function (response) {
-    debug('response')
     _.forEach(response.answers, function (answer) {
       if (answer.name === '_mm.local' && answer.type === 'TXT') {
-        debug('valid response')
         var nodeInfo = JSON.parse(answer.data.toString())
         // TODO: Validate incoming data
         self.hosts[nodeInfo.boxId] = nodeInfo
@@ -44,12 +40,10 @@ var mDNSService = function (options) {
 }
 
 mDNSService.prototype._requestAll = function (topic, local, data) {
-  debug('_requestAll')
   this.mdns.query('_mm.local', 'TXT')
 }
 
 mDNSService.prototype._update = function (topic, publicKey, data) {
-  debug('_update')
   this.nodeInfo = data
   this.mdns.query('_mm.local', 'TXT')
 }
